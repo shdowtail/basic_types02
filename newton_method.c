83e89ba9babb6e43ed15789184f9bfcf3631a6a0
alx <aleksandr.sinitson@outlook.com>
2022-12-23 23:03:29 +0200

    newtons_method finished

--- newton_method.c
+++ newton_method.c
@@ -1,47 +1,49 @@
+/* Program calculates square the square root of a number using the Newton's method.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
 #include <math.h>
-/* Broken state*/
+
 int main (void)
 {
   char *str_number;
   double x;
-
+  double y = 1.0f;
+  double y_old;
   double xy_div;
-  double x_xy_div_avg;
-
+  double y_xy_div_avg;
   char *end_pointer;
 
+  /* Allocating memory for a character array for reading input*/
   str_number = malloc (100);
   if (str_number == NULL)
-    {
-      printf ("Error: unable to allocate memory.\n");
-      return 1;
-    }
+	{
+	  printf ("Error: unable to allocate memory.\n");
+	  return 1;
+	}
   printf ("Enter a positive number: ");
   scanf ("%s", str_number);
+
+  /*Converting input-string to a double value*/
   x = strtod (str_number, &end_pointer);
   free (str_number);
 
-  double y = 1.0f;
-  double y_before = y;
+  /*First calculation happens outside a loop in order to assign right values for y_old and y*/
+  y_old = y;
   xy_div = x / y;
-  x_xy_div_avg = (x + xy_div) / 2.0f;
-  y = x_xy_div_avg;
-  printf ("   x       y       x/y   avg of y and x/y" );
-  printf ("%g %g %g %g\n", x, y, xy_div, x_xy_div_avg);
+  y_xy_div_avg = (y + xy_div) / 2;
+  y = y_xy_div_avg;
+  /*Loop repeats until absolute difference between y_old and y is less than 0.00001*/
+  while (fabs (y_old - y) > 0.00001)
+	{
+	  y_old = y;
+	  xy_div = x / y;
+	  y_xy_div_avg = (y + xy_div) / 2;
+	  y = y_xy_div_avg;
 
-  double y_after = y;
-  while (fabs(y_before - y_after))
-  {
-      y_before = y;
-      xy_div = x / y;
-      x_xy_div_avg = (x + xy_div) / 2.0f;
-      y = x_xy_div_avg;
-      y_after = y;
+	}
+  printf ("Square root = %g\n", y_xy_div_avg);
 
-      printf ("%g %g %g %g\n", x, y, xy_div, x_xy_div_avg);
-  }
+  return 0;
 }
