b2813482e6fb9b56eec34d7d9ce26e6ca5fd2465
alx <aleksandr.sinitson@outlook.com>
2022-12-23 21:36:03 +0200

    Revert "back again"
    This reverts commit 802cf1d026468e92e7af816fd8b4aaadfa48061a.

--- evaluation.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-//program evaluates expression 1+2.5*3 left to right
-int main (void)
-{
-  float expression = (1.0f + 2.5f) * 3.0f;
-  printf ("%.1f", expression);
-  return 0;
-}
